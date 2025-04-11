4101262ba0d5ce2c286188969cab55fe551d1d3b
Harshvardhansingh <harshitharsha778@gmail.com>
2025-04-11 19:45:48 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,8 +11,8 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <StrictMode>
       <Auth0Provider
-        domain={import.meta.env.VITE_AUTH_AUTH_DOMAIN}
-        clientId={import.meta.env.VITE_AUTH_AUTH_CLIENT_ID}
+        domain='dev-npy2j5sxru6o8h6n.us.auth0.com'
+        clientId='W9DgQFLZwsqVW129cxY78UncN88ETDBg'
         authorizationParams={{
           redirect_uri: window.location.origin
         }}
@@ -23,9 +23,3 @@ createRoot(document.getElementById('root')).render(
   </BrowserRouter>
 
 )
-// Hello Mam.
-// My Name is Harshvardhan Singh  came across your walk-in hiring post for the Junior Associate Full Stack Developer (MERN) role at Kainskep Solutions on April 12th.
-
-// Although I’m a fresher, I’ve worked extensively with the MERN stack and have built several projects, some of which I’ve also deployed live. Since the required skills and tech stack match my experience, I wanted to ask if I’m eligible to attend the walk-in drive.
-
-// Looking forward to your response. Thank you!
